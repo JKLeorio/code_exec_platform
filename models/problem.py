0a962622df9ff00721e211e90f3f14cfc6627ed7
JKLeorio <jkdplatinum@gmail.com>
2026-04-21 15:40:42 +0600

    deleted env, current project state

--- models/problem.py
+++ models/problem.py
@@ -1,5 +1,7 @@
+from typing import List, Text
+
 from sqlalchemy import Integer, String, Enum, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.types import SubmitStatus
 from models.base import Base
@@ -8,12 +10,39 @@ from models.execute_request import ExecuteRequest
 
 class Problem(Base):
     __tablename__ = "problems"
-
-    pass
+    description: Mapped[str] = mapped_column(
+        Text
+    )
+    expected_answer: Mapped[str] = mapped_column(
+        String
+    )
+    results: Mapped[List["Submit"]] = relationship(
+        back_populates="submits"
+    )
 
 
 class Submit(Base):
     __tablename__ = "submits"
     status: Mapped[SubmitStatus] = mapped_column(
+        Enum(SubmitStatus)
+    )
+    execute_request_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            "execute_results.id",
+            ondelete="CASCADE"
+        )
+    )
+    execute_request: Mapped[ExecuteRequest] = relationship(
+        ExecuteRequest
+    )
+    problem: Mapped[Problem] = relationship(
+        Problem
+    )
+    problem_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            "problems.id", ondelete="CASCADE"
+        )
+    )
+    status: SubmitStatus = mapped_column(
         Enum(SubmitStatus)
     )
